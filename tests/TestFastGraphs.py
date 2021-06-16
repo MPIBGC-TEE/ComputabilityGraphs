5b3aa0798ca307c8c239110daef69d5c02cd267f
Markus Müller <markus.mueller.1.g@googlemail.com>
2021-06-16 11:38:11 -0700

    added the first tests for a faster graph algorithm

---
+++ tests/TestFastGraphs.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+from frozendict import frozendict
+from copy import copy, deepcopy
+import networkx as nx
+from networkx.algorithms import bipartite
+from testinfrastructure.InDirTest import InDirTest
+from testinfrastructure.helpers import pp, pe
+from unittest import skip
+from copy import copy, deepcopy
+
+#from ComputabilityGraphs.graph_helpers import (
+#    arg_set_graph,
+#    initial_sparse_powerset_graph,
+#    minimal_startnodes_for_single_var,
+#    minimal_startnodes_for_node,
+#    update_step,
+#    toDiGraph,
+#    equivalent_singlegraphs,
+#    equivalent_multigraphs,
+#    node_2_string,
+#    nodes_2_string,
+#    edge_2_string,
+#    product_graph,
+#    sparse_powerset_graph,
+#    update_generator,
+#    # draw_multigraph_plotly,
+#    # draw_Graph_svg,
+#)
+#from ComputabilityGraphs.graph_plotting import (
+#    draw_update_sequence,
+#    draw_ComputerSetDiGraph_matplotlib,
+#    draw_ComputerSetMultiDiGraph_matplotlib,
+#)
+#from ComputabilityGraphs.non_graph_helpers import arg_set_set, all_mvars
+#
+from testComputers import  A, A1, A2, A3, A0, A_minus_1, A_minus_2, B, B1, B2, B3, B0, B_minus_1, B_minus_2, C, D, E, F, G, H, I, X, Y
+from testComputers import computers
+from testComputers import ( 
+    a_from_x,
+	b_from_y,
+	a_from_y,
+	b_from_x,
+	a_from_z,
+	b_from_z,
+	c_from_b,
+	d_from_b,
+	d_from_g_h,
+	a2_from_a1,
+	a3_from_a2,
+	b_minus_1_from_b_minus_2,
+    b0_from_b_minus_1,
+    a_minus_1_from_a_minus_2,
+    a1_from_a0,
+    a0_from_a_minus_1,
+    b1_from_b0,
+    b2_from_b1,
+    b3_from_b2,
+    a0_from_b0
+    )
+
+class TestFastGraphs(InDirTest):
+    def setUp(self):
+        self.computers = computers
+
+    def test_project(self):
+        # project the bipartite graph consisting of 
+        # two kinds of nodes (sets and decompositions)
+        # to the graph we actually need which has only sets
+
+        # var set
+        sn1 = frozenset([A3, B1])
+        sn11 = frozenset([A2, B0])
+        sn12 = frozenset([A2, B1])
+        sn13 = frozenset([A3, B0])
+        # decomposition
+        dn1 = (
+            frozenset([A3, B1]),    # active
+            frozenset([])           # passive
+        )
+        dn2 = (
+            frozenset([A3]),        # active
+            frozenset([B1])         # pass
+        )
+        dn3 = (
+            frozenset([B1]),        # active
+            frozenset([A3])         # pass
+        )
+        g = nx.DiGraph()
+        g.add_node(sn1, bipartite=0)
+
+        g.add_node(dn1, bipartite=1)
+        g.add_edge(dn1, sn1)
+        g.add_node(sn11, bipartite=0)
+
+        g.add_node(dn2, bipartite=1)
+        g.add_edge(dn2, sn1)
+        g.add_node(sn12, bipartite=0)
+
+        g.add_node(dn3, bipartite=1)
+        g.add_edge(dn3, sn1)
+        g.add_node(sn13, bipartite=0)
+
+        g.add_edge(
+            sn11,
+            dn1,
+            computers=frozenset({a3_from_a2, b1_from_b0})
+        )
+        g.add_edge(
+            sn12,
+            dn2,
+            computers=frozenset({a3_from_a2})
+        )
+        g.add_edge(
+            sn13,
+            dn3,
+            computers=frozenset({b1_from_b0})
+        )
+        (sets,partitions) = bipartite.sets(g) 
+        # or alternatively
+        top_nodes = {n for n, d in g.nodes(data=True) if d["bipartite"] == 0}
+
+        bottom_nodes = set(g) - top_nodes
+        #g_new = replaceNode(src=A2,repl=
+        G = bipartite.projected_graph(g, top_nodes)
+
--- tests/TestGraphs.py
+++ tests/TestGraphs.py
@@ -34,263 +34,270 @@ from ComputabilityGraphs.graph_plotting import (
 from ComputabilityGraphs.non_graph_helpers import arg_set_set, all_mvars
 
 
-class A_minus_1:
-    pass
-
-
-class A_minus_2:
-    pass
-
-
-class A:
-    """A variable we assume to be given"""
-
-    pass
-
-
-class B:
-    pass
-
-
-class A0:
-    pass
-
-
-class A1:
-    pass
-
-
-class A2:
-    pass
-
-
-class A3:
-    pass
-
-
-class B_minus_2:
-    pass
-
-
-class B_minus_1:
-    pass
-
-
-class B0:
-    pass
-
-
-class B1:
-    pass
-
-
-class B2:
-    pass
-
-
-class B3:
-    pass
-
-
-class C:
-    pass
-
-
-class D:
-    pass
-
-
-class E:
-    pass
-
-
-class F:
-    pass
-
-
-class G:
-    pass
-
-
-class H:
-    pass
-
-
-class I:
-    pass
-
-
-class X:
-    pass
-
-
-class Y:
-    pass
-
-
-class Z:
-    pass
-
-
-def a_from_b_c(b: B, c: C) -> A:
-    return A()
-
-
-def a_from_b_d(b: B, d: D) -> A:
-    return A()
-
-
-def a_from_x(x: X) -> A:
-    return A()
-
-
-def b_from_x(x: X) -> B:
-    return B()
-
-
-def a_from_y(y: Y) -> A:
-    return A()
-
-
-def b_from_y(y: Y) -> B:
-    return B()
-
-
-def a_from_z(z: Z) -> A:
-    return A()
-
-
-def b_from_z(z: Z) -> B:
-    return B()
-
-
-def c_from_z(z: Z) -> C:
-    return C()
-
-
-def a_from_i(i: I) -> A:
-    """Computes a from i"""
-    return A()
-
-
-def b_from_c_d(c: C, d: D) -> B:
-    return B()
-
-
-def b_from_d_e(d: D, e: E) -> B:
-    return B()
-
-
-def b_from_e_f(e: E, f: F) -> B:
-    return B()
-
-
-def c_from_b(b: B) -> C:
-    """Computes c from b"""
-    return C()
-
-
-def d_from_a(a: A) -> D:
-    return D()
-
-
-def d_from_b(b: B) -> D:
-    """Computes d from b"""
-    return D()
-
-
-def d_from_g_h(g: G, h: H) -> D:
-    """Computes d from g and h"""
-    return D()
-
-
-def e_from_b(b: B) -> E:
-    """Computes e from b"""
-    return E()
-
-
-def f_from_b(b: B) -> F:
-    """Computes f from b"""
-    return F()
-
-
-def a_minus_1_from_a_minus_2(x: A_minus_2) -> A_minus_1:
-    return A_minus_1()
-
-
-def a0_from_a_minus_1(x: A_minus_1) -> A0:
-    return A0()
-
-
-def a1_from_a0(a0: A0) -> A1:
-    return A1()
-
-
-def a2_from_a1(a1: A1) -> A2:
-    return A2()
-
-
-def a3_from_a2(a2: A2) -> A3:
-    return A3()
-
-
-def a0_from_b0(x: B0) -> A0:
-    return A0()
-
-
-def b_minus_1_from_b_minus_2(x: B_minus_2) -> B_minus_1:
-    return B_minus_1()
-
-
-def b0_from_b_minus_1(x: B_minus_1) -> B0:
-    return B0()
-
-
-def b1_from_b0(b0: B0) -> B1:
-    return B1()
-
-
-def b2_from_b1(b1: B1) -> B2:
-    return B2()
-
-
-def b3_from_b2(b2: B2) -> B3:
-    return B3()
-
-
-# for easier debugging in ipython
-computers = frozenset(
-    {
-        a_from_i,
-        b_from_c_d,
-        b_from_e_f,
-        c_from_b,
-        d_from_b,
-        d_from_g_h,
-        e_from_b,
-        f_from_b,
-    }
-)
-
-
+#class A_minus_1:
+#    pass
+#
+#
+#class A_minus_2:
+#    pass
+#
+#
+#class A:
+#    """A variable we assume to be given"""
+#
+#    pass
+#
+#
+#class B:
+#    pass
+#
+#
+#class A0:
+#    pass
+#
+#
+#class A1:
+#    pass
+#
+#
+#class A2:
+#    pass
+#
+#
+#class A3:
+#    pass
+#
+#
+#class B_minus_2:
+#    pass
+#
+#
+#class B_minus_1:
+#    pass
+#
+#
+#class B0:
+#    pass
+#
+#
+#class B1:
+#    pass
+#
+#
+#class B2:
+#    pass
+#
+#
+#class B3:
+#    pass
+#
+#
+#class C:
+#    pass
+#
+#
+#class D:
+#    pass
+#
+#
+#class E:
+#    pass
+#
+#
+#class F:
+#    pass
+#
+#
+#class G:
+#    pass
+#
+#
+#class H:
+#    pass
+#
+#
+#class I:
+#    pass
+#
+#
+#class X:
+#    pass
+#
+#
+#class Y:
+#    pass
+#
+#
+#class Z:
+#    pass
+#
+#
+#def a_from_b_c(b: B, c: C) -> A:
+#    return A()
+#
+#
+#def a_from_b_d(b: B, d: D) -> A:
+#    return A()
+#
+#
+#def a_from_x(x: X) -> A:
+#    return A()
+#
+#
+#def b_from_x(x: X) -> B:
+#    return B()
+#
+#
+#def a_from_y(y: Y) -> A:
+#    return A()
+#
+#
+#def b_from_y(y: Y) -> B:
+#    return B()
+#
+#
+#def a_from_z(z: Z) -> A:
+#    return A()
+#
+#
+#def b_from_z(z: Z) -> B:
+#    return B()
+#
+#
+#def c_from_z(z: Z) -> C:
+#    return C()
+#
+#
+#def a_from_i(i: I) -> A:
+#    """Computes a from i"""
+#    return A()
+#
+#
+#def b_from_c_d(c: C, d: D) -> B:
+#    return B()
+#
+#
+#def b_from_d_e(d: D, e: E) -> B:
+#    return B()
+#
+#
+#def b_from_e_f(e: E, f: F) -> B:
+#    return B()
+#
+#
+#def c_from_b(b: B) -> C:
+#    """Computes c from b"""
+#    return C()
+#
+#
+#def d_from_a(a: A) -> D:
+#    return D()
+#
+#
+#def d_from_b(b: B) -> D:
+#    """Computes d from b"""
+#    return D()
+#
+#
+#def d_from_g_h(g: G, h: H) -> D:
+#    """Computes d from g and h"""
+#    return D()
+#
+#
+#def e_from_b(b: B) -> E:
+#    """Computes e from b"""
+#    return E()
+#
+#
+#def f_from_b(b: B) -> F:
+#    """Computes f from b"""
+#    return F()
+#
+#
+#def a_minus_1_from_a_minus_2(x: A_minus_2) -> A_minus_1:
+#    return A_minus_1()
+#
+#
+#def a0_from_a_minus_1(x: A_minus_1) -> A0:
+#    return A0()
+#
+#
+#def a1_from_a0(a0: A0) -> A1:
+#    return A1()
+#
+#
+#def a2_from_a1(a1: A1) -> A2:
+#    return A2()
+#
+#
+#def a3_from_a2(a2: A2) -> A3:
+#    return A3()
+#
+#
+#def a0_from_b0(x: B0) -> A0:
+#    return A0()
+#
+#
+#def b_minus_1_from_b_minus_2(x: B_minus_2) -> B_minus_1:
+#    return B_minus_1()
+#
+#
+#def b0_from_b_minus_1(x: B_minus_1) -> B0:
+#    return B0()
+#
+#
+#def b1_from_b0(b0: B0) -> B1:
+#    return B1()
+#
+#
+#def b2_from_b1(b1: B1) -> B2:
+#    return B2()
+#
+#
+#def b3_from_b2(b2: B2) -> B3:
+#    return B3()
+#
+#
+## for easier debugging in ipython
+#computers = frozenset(
+#    {
+#        a_from_i,
+#        b_from_c_d,
+#        b_from_e_f,
+#        c_from_b,
+#        d_from_b,
+#        d_from_g_h,
+#        e_from_b,
+#        f_from_b,
+#    }
+#)
+
+from testComputers import  A, A1, A2, A3, A0, A_minus_1, A_minus_2, B, B1, B2, B3, B0, B_minus_1, B_minus_2, C, D, E, F, G, H, I, X, Y
+from testComputers import computers
+from testComputers import ( 
+    a_from_x,
+	b_from_y,
+	a_from_y,
+	b_from_x,
+	a_from_z,
+	b_from_z,
+	c_from_b,
+	d_from_b,
+	d_from_g_h,
+	a2_from_a1,
+	a3_from_a2,
+	b_minus_1_from_b_minus_2,
+    b0_from_b_minus_1,
+    a_minus_1_from_a_minus_2,
+    a1_from_a0,
+    a0_from_a_minus_1,
+    b1_from_b0,
+    b2_from_b1,
+    b3_from_b2,
+    a0_from_b0
+    )
 class TestGraphs(InDirTest):
     def setUp(self):
-        # fixme mm 02-03-2020:
-        # assemble this set from a file of class definitions
-        self.mvars = {
-            A,
-            B,
-            C,
-            D,
-            E,
-            F,
-            G,
-            H,
-            I,
-        }
-        # fixme mm 02-03-2020:
-        # assemble this set from a file of annotated functions in
-        # special file
         self.computers = computers
 
     def test_toDiGraph(self):
