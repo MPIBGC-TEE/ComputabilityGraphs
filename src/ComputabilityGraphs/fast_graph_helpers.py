fd640791668b1cae0dde5d91a3dc534a1c44d890
Markus Mueller <markus.mueller.1.g@googlemail.com>
2022-12-29 18:14:43 -0500

    the lru_cache decorator seems to anoy python 3.7 (used by binder)

--- src/ComputabilityGraphs/fast_graph_helpers.py
+++ src/ComputabilityGraphs/fast_graph_helpers.py
@@ -330,7 +330,7 @@ def add_all_decompositions_to_all_nodes(
     return g_res, all_new_decompositions
 
 
-@lru_cache
+#@lru_cache
 def fast_graph(
         root_type: type,
         cs: ComputerSet,
