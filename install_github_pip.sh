e4704b0540033d13e3c36a2f0e02d3fd034517ca
Markus Müller <markus.mueller.1.g@googlemail.com>
2025-07-03 17:45:01 +0200

    added a new test for the github only install

---
+++ install_github_pip.sh
@@ -0,0 +1,3 @@
+
+pip install -e git+https://github.com/MPIBGC-TEE/testinfrastructure.git#egg=testinfrastructure
+pip install -e git+https://github.com/MPIBGC-TEE/ComputabilityGraphs.git#egg=ComputabilityGraphs
